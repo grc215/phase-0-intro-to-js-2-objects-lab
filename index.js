548e759929a72c1d43b47e805b3fd84270b13039
Greg <caldwell.gregoryj@gmail.com>
2021-03-28 20:45:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const employee = {
+    Name: 'Mitch',
+    streetaddress: '855',
+  };
+  function dupdateEmployeeWithKeyAndValue(employee, key, value) {
+    obj[key] = value; //Why are we using bracket notation here?
+  
+    return obj;
+  } 
